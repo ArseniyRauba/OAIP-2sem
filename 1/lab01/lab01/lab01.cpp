429e511af3f75c3cb6ea7a2dfd2c1f4f93d4b5fb
Arseniy Rauba <155465667+ArseniyRauba@users.noreply.github.com>
2025-01-28 17:59:58 +0300

    Add files via upload

---
+++ 1/lab01/lab01/lab01.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+int GetRoot1() //функция для вычисления корня первого уравнения
+{
+	setlocale(LC_ALL, "rus");
+	double a = -100, b = 100, e = 0.001, x;
+	cout << "Введите а:";
+	cin >> a;
+	cout << "Введите b:";
+	cin >> b;
+
+	//вычисления для метода дихотомии
+	while ((abs(a - b)) > 2 * e)
+	{
+		x = (a + b) / 2;
+
+		if ((pow(x, 3) + x - 4) * (pow(a, 3) + a - 4) <= 0) // подстановка уравнения
+		{
+			b = x;
+		}
+		else a = x;
+	} 
+
+	cout << "Корень уравнения " << x << endl;
+	return 0;
+}
+
+int GetRoot2() //функция для вычисления корня второго уравнения
+{
+	setlocale(LC_ALL, "rus");
+	double a = -100, b = 100, e = 0.001, x;
+	cout << "Введите а:";
+	cin >> a;
+	cout << "Введите b:";
+	cin >> b;
+
+	//вычисления для метода дихотомии
+	while ((abs(a - b)) > 2 * e)
+	{
+		x = (a + b) / 2;
+
+		if (((1-pow(x,2)) * (1-pow(a,2))) <= 0) // подстановка уравнения
+		{
+			b = x;
+		}
+		else a = x;
+	} 
+
+	cout << "Корень уравнения " << x << endl;
+	return 0;
+}
+
+//функция указателя
+void ShowResults(int(*pointer)())
+{
+	cout << pointer() << endl;
+}
+
+//использование указателя для выбора уравнения
+void main()
+{
+	ShowResults(GetRoot2);
+}
---
+++ 1/lab01/zadanie2/zadanie2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cstdarg>
+using namespace std;
+
+int maxWordLength(const char* sentence, ...) { //функция для вычисления количества букв в длинном слове
+    va_list args; //создание списка аргументов переменной длины
+    va_start(args, sentence); //инициализация va_list, начальное положение для доступа к аргументам
+
+    int max_length = 0;
+    const char* word;
+
+    while ((word = va_arg(args, const char*)) != NULL) { //получение аргумента для условия проверки
+        int length = strlen(word);
+        if (length > max_length) { //нахождение максимальной длины слова
+            max_length = length;
+        }
+    }
+
+    va_end(args); //завершение работы со списком аргументов
+
+    return max_length;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "rus");
+
+    const char* sentence1 = "привет как у тебя дела";
+    const char* sentence2 = "очень короткое предложение";
+    const char* sentence3 = "сегодня вторник";
+
+    cout << "1 предложение: привет как у тебя дела" << endl;
+    cout << "2 предложение: очень короткое предложение" << endl;
+    cout << "3 предложение: сегодня вторник" << endl;
+
+    cout << "Самое длинное слово в первом предложении: " << maxWordLength("привет", "как", "у", "тебя", "дела", NULL) << std::endl; //первый вызов функции
+    cout << "Самое длинное слово во втором предложении: " << maxWordLength("очень", "короткое", "предложение", NULL) << std::endl; //второй вызов функции
+    cout << "Самое длинное слово в третьем предложении: " << maxWordLength("сегодня", "вторник", NULL) << std::endl; //третий вызов функции
+
+    return 0;
+}
